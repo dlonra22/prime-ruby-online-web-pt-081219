0ead4ed664bc03efbdb46014d9480c17889bc18e
arnie b <arnold.bwaila@gmail.com>
2019-08-24 21:34:38 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,7 +3,8 @@ def prime?(integer)
   cnt = 2 
   if integer > 0 
     lastnumber = Integer.sqrt(integer).to_i
-  
+  else return false 
+  end
     range = (2..lastnumber).to_a
   
   range.all? do |diviser|
